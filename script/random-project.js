b1404bc5078cb88f75f384dd21061c9811891a5b
DevRenamon-Alder <devrenamon.alder@gmail.com>
2025-03-12 14:23:31 -0500

    Add Random Project Generator Script, embed it to projects.html, edit index.html pinboard

---
+++ script/random-project.js
@@ -0,0 +1,15 @@
+let projectNum = 0;
+let title = document.getElementById("title");
+let description = document.getElementById("description");
+let link = document.getElementById("link");
+
+const randomizeProject = () => {
+    projectNum = Math.floor(Math.random() * 0);
+    if (projectNum === 0) {
+        title.innerHTML = "Random Girl Generator";
+        description.innerHTML = "This project generates a random girl, complete with a physical description and personality description.";
+        link.innerHTML = "<a href='#'>Random Girl Generator</a>"
+    } else {
+        console.log("invalid projectNum");
+    }
+}
