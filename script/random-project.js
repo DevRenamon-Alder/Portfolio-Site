4d2e53e8a94b35069242cd6ee93de363f2c0b418
DevRenamon-Alder <devrenamon.alder@gmail.com>
2025-03-18 12:34:46 -0500

    Add Random Girl Generator Link

--- script/random-project.js
+++ script/random-project.js
@@ -8,7 +8,7 @@ const randomizeProject = () => {
     if (projectNum === 0) {
         title.innerHTML = "Random Girl Generator";
         description.innerHTML = "This project generates a random girl, complete with a physical description and personality description.";
-        link.innerHTML = "<a href='#'>Random Girl Generator</a>"
+        link.innerHTML = "<a href='https://devrenamon-alder.github.io/Random-Girl-Generator/'>Random Girl Generator</a>"
     } else {
         console.log("invalid projectNum");
     }
